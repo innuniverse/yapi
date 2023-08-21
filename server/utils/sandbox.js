f794bd49506f091318b34632c10d9993e81c1424
innuniverse <innuniverse>
2023-08-21 08:09:35 +0800

    #2695 修复沙盒导致高级mock和自动化等模块出现bug

--- server/utils/sandbox.js
+++ server/utils/sandbox.js
@@ -1,17 +1,34 @@
 const Safeify = require('safeify').default;
 
-module.exports = async function sandboxFn(context, script) {
+module.exports = async function sandboxFn(context, script, autoTest) {
     // 创建 safeify 实例
     const safeVm = new Safeify({
-        timeout: 3000,
-        asyncTimeout: 60000
-    })
+        timeout: 3000,          //超时时间
+        unrestricted: true, // true为不受CPU限制，以解决Docker启动问题
+        asyncTimeout: 10000,    //包含异步操作的超时时间
+        quantity: 4,          //沙箱进程数量，默认同 CPU 核数
+        memoryQuota: 500,     //沙箱最大能使用的内存（单位 m），默认 500m
+        cpuQuota: 0.5,        //沙箱的 cpu 资源配额（百分比），默认 50%
+        unsafe: {
+            modules: {
+                assert: 'assert', // 引入assert断言库
+                mockjs: 'mockjs'
+            }
+        }
+    });
+    safeVm.preset('const assert = require("assert");const Mock = require("mockjs"); const Random = Mock.Random;');
 
-    script += "; return this;";
     // 执行动态代码
+    if (autoTest) {
+        script += "\n return {}";
+    } else {
+        script += "\n  return {mockJson, resHeader, httpCode, delay}";
+    }
     const result = await safeVm.run(script, context)
 
+
     // 释放资源
-    safeVm.destroy()
+    safeVm.destroy();
     return result
-}
+};
+
